c310caeb9f08e9beeee9dfbe30453b8203919074
tawan <87tawanzinho@gmail.com>
2023-12-22 18:53:42 -0300

    fix bug h

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   }
   return (
   <main className='css-bg h-screen flex justify-center items-center'>
-      <div className='h-2/4  lg:h-2/4 w-screen sm:w-4/5 lg:w-1/2 bg-white p-8 gap-10 flex flex-col '>
+      <div className='lg:h-2/4 w-screen sm:w-4/5 lg:w-1/2 bg-white p-8 gap-10 flex flex-col '>
           <div className='flex flex-col'>
             <p className='text-gray-900 text-2xl font-bold'>Registrar</p>
             <p className='text-gray-700 font-bold'>Vamos criar sua conta</p>
